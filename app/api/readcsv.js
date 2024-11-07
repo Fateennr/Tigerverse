7d4e64f0f02c88c7ac80c05ab5a2e04f9c064ce1
fateennr <nnaher123@gmail.com>
2024-11-08 00:05:35 +0600

    fixed

--- app/api/readcsv.js
+++ app/api/readcsv.js
@@ -10,37 +10,30 @@ const filePath = 'players.csv';
 // console.log(`${ filePath }`);
 
 fs.createReadStream(filePath)
-    .pipe(csv())
-//     .on('data', (row) => {
-
-//         if(players.length === 0)
-//             {
-//                 console.log('no player found');
-//             }
-//         // players.push({
-//         //     Name : row['Name'],
-//         //     DOB : new Date(row['DOB']), // using new Date() to convert the date into ISO-8601 format
-//         //     BattingStyle : row['BattingStyle'],
-//         //     ICCRanking : parseInt(row['ICCRanking'], 10), // have to parse to int as we are getting string from the csv, 10 is the radix
-//         //     IntDebut : new Date(row['IntDebut']),
-//         //     Profile : row['Profile'],
-//         //     CaptainStatus : row['CaptainStatus'] === true, //converting the String to boolean
-//         //     PlayerRole : row['PlayerRole']
-//         // });
-//     })
-    .on('end', async () =>{
-
-        console.log('Parsed Data: ');
-        console.log(players);
-
-
-
+.pipe(csv())
+.on('data', (row) => 
+{
+        // players.push(data);
+        players.push({
+            Name : row['Name'],
+            DOB : new Date(row['DOB']), // using new Date() to convert the date into ISO-8601 format
+            BattingStyle : row['BattingStyle'],
+            ICCRanking : parseInt(row['ICCRanking'], 10), // have to parse to int as we are getting string from the csv, 10 is the radix
+            IntDebut : new Date(row['IntDebut']),
+            Profile : row['Profile'],
+            CaptainStatus : row['CaptainStatus'] === true, //converting the String to boolean
+            PlayerRole : row['PlayerRole']
+        });
+})
+.on('end', async () =>
+{
+        console.log(players); 
         for (const player of players) {
-            await prisma.player.create( { data: player } );
+            await prisma.player.create( { data: player } ); //parsing from csv, to prisma
         }
-        console.log('Data Imported!');
+
         await prisma.$disconnect();
-    })
-    .on('error', (err) =>{
-        console.error('Error while parsing CSV', err);
-    });
+})
+.on('error', (err) =>{
+    console.error('Error while parsing CSV', err);
+});
