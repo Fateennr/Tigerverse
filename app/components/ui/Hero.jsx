cc7d58c14d895b8c9d1ae73c3a464516308f3378
fateennr <nnaher123@gmail.com>
2025-01-30 16:31:58 +0600

    new ui

--- app/components/ui/Hero.jsx
+++ app/components/ui/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero() {
     return (
         <>
             <div className="hero">
-                <svg width= "100%" height="1000px" xmlns="http://www.w3.org/2000/svg">
+                <svg width= "100%" height="900px" xmlns="http://www.w3.org/2000/svg">
                     <defs>
                         <linearGradient id="heroGradient" x1="0%" y1="0%" x2="100%" y2="0%">
                             <stop offset="0%" stopColor="#00520A" />
@@ -26,16 +26,16 @@ export default function Hero() {
                 <svg width= "100%" height="400px" xmlns="http://www.w3.org/2000/svg" style={{ position: "relative", top: "-400px" }}>
                     <defs>
                         {/* <linearGradient id="herobottomGradient" x1="0%" y1="0%" x2="0%" y2="100%"> */}
-                        <radialGradient id="circularFade" cx="50%" cy="0%" r="115%" fx="50%" fy="0%">
-                            <stop offset="0%" stopColor="#293325" stopOpacity="0"/>
+                        <radialGradient id="circularFade" cx="50%" cy="0%" r="100%" fx="50%" fy="0%">
+                            <stop offset="0%" stopColor="#1c1c1c" stopOpacity="0"/>
                             <stop offset="10%" stopColor="#293325" stopOpacity="0.01"/>
                             <stop offset="40%" stopColor="#293325" stopOpacity="0.02"/>
                             <stop offset="50%" stopColor="#293325" stopOpacity="0.05"/>
                             <stop offset="60%" stopColor="#181f18" stopOpacity="0.2"/>
                             <stop offset="70%" stopColor="#181f18" stopOpacity="0.3"/>
-                            <stop offset="80%" stopColor="#00520A" stopOpacity="0.4"/>
-                            <stop offset="90%" stopColor="white" stopOpacity="0.8"/>
-                            <stop offset="95%" stopColor="white" stopOpacity="1"/>
+                            <stop offset="80%" stopColor="#00520A" stopOpacity="0.5"/>
+                            <stop offset="90%" stopColor="#999999" stopOpacity="0.8"/>
+                            <stop offset="95%" stopColor="white" stopOpacity="0.9"/>
                             <stop offset="100%" stopColor="white" stopOpacity="1"/>
                         </radialGradient>
 
