0be10d72db83197097c35193500551423b879573
Nowshiningg <141920616+Nowshiningg@users.noreply.github.com>
2025-04-29 12:02:53 +0600

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const playerRoutes = require('./routes/player.routes');
-const squadRoutes = require('./routes/squad.routes');
-const servicesRoutes = require('./routes/services.routes');
-const matchesRoutes = require('./routes/matches.routes');
+// index.js
 
+const express        = require('express');
+const cors           = require('cors');
+
+// import your routers before you ever call app.use()
+const playerRoutes   = require('./routes/player.routes');
+const squadRoutes    = require('./routes/squad.routes');
+const servicesRoutes = require('./routes/services.routes');
+const galleryRoutes  = require('./routes/gallery.routes');
+
+const app = express();
+
+// now you can safely mount them
 app.use(cors({
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   credentials: true
 }));
-
 app.use(express.json());
 
-app.use('/players', playerRoutes);
-app.use('/squad', squadRoutes);
+app.use('/players',  playerRoutes);
+app.use('/squad',    squadRoutes);
 app.use('/services', servicesRoutes);
-app.use('/matches', matchesRoutes);
+app.use('/gallery',  galleryRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
